15adf0a80808dc5577ad6fa40449b146ebaa8c93
George Talusan <george.talusan@gmail.com>
2018-09-10 10:48:41 -0400

    first commit

---
+++ torstar.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         The Toronto Star Meter Wall Remover
+// @supportURL   http://github.com/gtalusan/tampermonkey-torstar
+// @namespace    http://github.com/gtalusan/
+// @version      0.1
+// @description  remove the meter div
+// @author       You
+// @match        https://www.thestar.com/*/*.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(function() {
+        var meter = document.getElementsByClassName('articleMeterWall')[0];
+        if (!meter) {
+            console.log("no meter wall");
+            return;
+        }
+        var meterParent = meter.parentNode; /* get the empty div */
+        var meterGrandparent = meterParent.parentNode;
+        meterGrandparent.removeChild(meterParent);
+    }, 1000);
+})();
