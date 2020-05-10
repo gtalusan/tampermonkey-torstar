3dc5b5e4cb0e90f1fa48050270cf221777e8d8f3
George Talusan <george.talusan@gmail.com>
2020-05-10 17:32:45 -0400

    0.6

--- torstar.js
+++ torstar.js
@@ -2,46 +2,17 @@
 // @name         The Toronto Star Meter Wall Remover
 // @supportURL   http://github.com/gtalusan/tampermonkey-torstar
 // @namespace    http://github.com/gtalusan/
-// @version      0.5
+// @version      0.6
 // @description  remove the meter div
 // @author       You
 // @match        https://www.thestar.com
 // @match        https://www.thestar.com/*/*.html*
 // @grant        none
 // @run-at       document-idle
-// @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    setTimeout(function() {
-        var meter = document.getElementsByClassName('articleMeterWall')[0];
-        if (!meter) {
-           console.log("no meter wall");
-           return;
-        }
-        var meterParent = meter.parentNode; /* get the empty div */
-        var meterGrandparent = meterParent.parentNode;
-        meterGrandparent.removeChild(meterParent);
-    }, 1000);
-    setTimeout(function() {
-        $('p').filter(function() {
-            return $(this).css('display') == 'none' && !$(this).hasClass('print-header') && !$(this).hasClass('print-footer');
-        }).css('display', 'block');
-    }, 1000);
-    setTimeout(function() {
-        $('div').filter(function() {
-            return $(this).hasClass('basic-paywall-new');
-        }).remove();
-    }, 1000);
-    setTimeout(function() {
-        var banner = document.getElementsByClassName('meter-banner')[0];
-        if (!banner) {
-            console.log("no banner");
-            return;
-        }
-        var bannerParent = banner.parentNode;
-        bannerParent.removeChild(banner);
-    }, 1000);
-})();
+window.__PRELOADED_CONFIG_DATA__.meterCounterName = "";
+window.__PRELOADED_CONFIG_DATA__.meterMppApiCall = "";
+window.__PRELOADED_CONFIG_DATA__.overlay = {};
+window.__PRELOADED_CONFIG_DATA__.overlayRenew = {};
+window.__PRELOADED_CONFIG_DATA__.overlaySubscriber = {};
