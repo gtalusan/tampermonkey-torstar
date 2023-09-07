39a69f9194c24d761abeefeb0ff6c5632d358504
George Talusan <george.talusan@gmail.com>
2023-09-07 10:39:17 -0400

    remove more annoying crap

--- torstar-3.js
+++ torstar-3.js
@@ -2,8 +2,8 @@
 // @name         The Toronto Star Subscriber Wall Remover
 // @supportURL   http://github.com/gtalusan/tampermonkey-torstar
 // @namespace    http://github.com/gtalusan/
-// @version      0.6
-// @description  remove the meter div
+// @version      0.7
+// @description  remove the meter div and re-block/unhide subscriber-only elements
 // @author       You
 // @match        https://www.thestar.com
 // @match        https://www.thestar.com/*/*.html*
@@ -13,3 +13,15 @@
 // ==/UserScript==
 
 $(".ReactModalPortal").remove();
+setTimeout(function() {
+    $(".subscriber-only").each(function() {
+        $(this).removeAttr("hidden");
+        $(this).css("display", "block");
+    });
+    $(".subscriber-offers").each(function() {
+        $(this).remove();
+    });
+    $(".subscriber-preview").each(function() {
+        $(this).removeClass("last-preview");
+    });
+}, 1500);
