0a704d20fa379ac1218bc926fff58d3a13e9250a
George Talusan <george.talusan@gmail.com>
2024-12-05 15:59:13 -0500

    come on

--- torstar-3.js
+++ torstar-3.js
@@ -2,7 +2,7 @@
 // @name         The Toronto Star Subscriber Wall Remover
 // @supportURL   http://github.com/gtalusan/tampermonkey-torstar
 // @namespace    http://github.com/gtalusan/
-// @version      0.7
+// @version      0.8
 // @description  remove the meter div and re-block/unhide subscriber-only elements
 // @author       You
 // @match        https://www.thestar.com
@@ -13,15 +13,19 @@
 // ==/UserScript==
 
 $(".ReactModalPortal").remove();
-setTimeout(function() {
+setInterval(function() {
     $(".subscriber-only").each(function() {
         $(this).removeAttr("hidden");
         $(this).css("display", "block");
+        $(this).removeClass("subscriber-only");
     });
     $(".subscriber-offers").each(function() {
         $(this).remove();
     });
     $(".subscriber-preview").each(function() {
         $(this).removeClass("last-preview");
+    });
+    $(".tsArticle").each(function() {
+        $(this).removeAttr("hidden");
     });
 }, 1500);
